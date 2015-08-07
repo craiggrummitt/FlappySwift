505ffb6be594cc7ab1dd12089cf578a97b42465e
craiggrummitt <craiggrummitt@gmail.com>
2015-08-07 17:24:09 -0500

    Initial Commit

---
+++ FlappySwiftTests/FlappySwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  FlappySwiftTests.swift
+//  FlappySwiftTests
+//
+//  Created by Craig on 7/08/2015.
+//  Copyright (c) 2015 InteractiveCoconut. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class FlappySwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
